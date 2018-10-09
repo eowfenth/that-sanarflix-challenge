7059e5c1c92da11007fa4640ad8d994892376bf2
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2018-10-09 18:59:53 -0300

    refactor(test): use it keyword instead of test

--- App.test.tsx
+++ App.test.tsx
@@ -3,7 +3,7 @@ import 'react-native';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders correctly', () => {
+it('renders correctly', () => {
 	const tree = renderer.create(<App />);
 	expect(tree).toBeDefined();
 });
