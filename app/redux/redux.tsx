5e7bc7ee18b3e3586f7cc288854993dff50b1941
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2018-10-11 01:04:14 -0300

    feat(redux): setup reconciler into project

--- app/redux/redux.tsx
+++ app/redux/redux.tsx
@@ -1,14 +1,33 @@
 import { combineReducers, createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import {
+	seamlessImmutableReconciler,
+	seamlessImmutableTransformCreator,
+} from './reconciler';
 
-export const reducers = combineReducers({});
+import { reducer } from './main';
+
+export const reducers = combineReducers({
+	main: reducer,
+});
+
+const transformerConfig = {
+	whitelistPerReducer: {
+		reducerA: ['keyA', 'keyB'],
+	},
+	blacklistPerReducer: {
+		reducerB: ['keyC', 'keyD'],
+	},
+};
 
 const rootReducer = persistReducer(
 	{
 		storage,
 		version: 1,
 		key: 'storage',
+		stateReconciler: seamlessImmutableReconciler,
+		transforms: [seamlessImmutableTransformCreator(transformerConfig)],
 	},
 	reducers
 );
